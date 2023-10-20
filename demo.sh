10d60050078b5868c5c7a16820de9d8078a32166
Iain Samuel McLean Elder <iain@isme.es>
2023-10-21 01:10:52 +0200

    Set autoescape in stack group config

--- demo.sh
+++ demo.sh
@@ -7,7 +7,7 @@ export PYTHONWARNINGS=ignore
 env --chdir=test/templates python3 -m http.server --bind "127.0.0.1" &> /dev/null &
 http_pid=$!
 
-find test/config -mindepth 2 -printf '%P\0' \
+find test/config -mindepth 2 ! -name config.yaml -printf '%P\0' \
 | sort -z \
 | xargs -t -0 -n1 env --chdir=test sceptre --output=yaml generate
 
