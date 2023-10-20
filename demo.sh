0557dccdd603c682fe4b7f752ac47dabe76be7df
Iain Samuel McLean Elder <iain@isme.es>
2023-10-21 01:30:01 +0200

    Don't care about config depth

--- demo.sh
+++ demo.sh
@@ -6,7 +6,7 @@ export PYTHONWARNINGS=ignore
 
 env --chdir=test/templates python3 -m http.server --bind "127.0.0.1" &> /dev/null &
 
-find test/config -mindepth 2 ! -name config.yaml -printf '%P\0' \
+find test/config -type f ! -name config.yaml -printf '%P\0' \
 | sort -z \
 | xargs -t -0 -n1 env --chdir=test sceptre --output=yaml generate
 
