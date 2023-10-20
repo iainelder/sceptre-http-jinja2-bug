a4bd74e4c34d1559e0091e32d06505ee7de63cf4
Iain Samuel McLean Elder <iain@isme.es>
2023-10-21 01:29:16 +0200

    Kill HTTP server by name

--- demo.sh
+++ demo.sh
@@ -5,10 +5,9 @@ set -euo pipefail
 export PYTHONWARNINGS=ignore
 
 env --chdir=test/templates python3 -m http.server --bind "127.0.0.1" &> /dev/null &
-http_pid=$!
 
 find test/config -mindepth 2 ! -name config.yaml -printf '%P\0' \
 | sort -z \
 | xargs -t -0 -n1 env --chdir=test sceptre --output=yaml generate
 
-kill "$http_pid"
+pkill -f http.server
